450a3f8dd6681241cea71193ff14ce4a7a372c1c
IoanaGeorgia <91886439+IoanaGeorgia@users.noreply.github.com>
2023-07-30 12:54:18 +0300

    title added

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <LandingPage />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LandingPage from './components/LandingPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    LandingPage
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  /* font-family:; */
 }
 </style>
---
+++ src/components/LandingPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+  <p class='githubJobsTitle'><span class='githubJobsSpan'>Github</span>Jobs</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'LandingPage',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+</style>
